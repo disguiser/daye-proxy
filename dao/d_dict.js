97b2708024c6faad47a737cd0d7b76d265897d7f
zhoumingshuai <zhoumingshuai@gmail.com>
2017-03-06 13:36:58 +0800

    抵质押物录入流程

--- dao/d_dict.js
+++ dao/d_dict.js
@@ -12,7 +12,7 @@ let find_guarantor_dict = async (dict_id, next) => {
 }
 let get_contract_id = async (regitem_id, next) => {
     let contract_id = [];
-    await sequelize.query(`select DB_CONTRACT_ID from INTRUSTQLC..QLC_TASSURE_CONTRACT where REGITEM_ID =1986 and isnull(ASSURE_BH,'')<>''`, {
+    await sequelize.query(`select DB_CONTRACT_ID from INTRUSTQLC..QLC_TASSURE_CONTRACT where REGITEM_ID =${regitem_id} and isnull(ASSURE_BH,'')<>''`, {
         type: sequelize.QueryTypes.SELECT
     }).then(function(data){
         data.forEach(function(element){

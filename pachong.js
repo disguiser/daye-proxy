d59797e6cc91a3b30266ed9befc0f7ae7706b9e0
zhoumingshuai <zhoumingshuai@gmail.com>
2017-05-15 16:44:56 +0800

    Revert "uuid变更"
    This reverts commit 1a1be4cea51297414ed16e4f10e85718be85405f.

--- pachong.js
+++
@@ -1,8 +0,0 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-
-(async () => {
-    let html = await rp(`https://cnodejs.org/`);
-    let $ = cheerio(html);
-    console.log(html);
-})();
--- test-db.js
+++ test-db.js
@@ -1,5 +1,8 @@
 require('babel-core/register')({
     presets: ['stage-3']
 });
-require('./pachong');
+// require('./httprequest');
+const moment = require('moment');
+
+console.log(moment().format('YYYY-MM-DD hh:mm:ss'));
 

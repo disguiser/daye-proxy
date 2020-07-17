9f57ba427f37ec358458f9194664e0fa0aba4623
sunhuan <303864974@qq.com>
2020-07-17 16:45:34 +0800

    测试完善监管报送系统功能.

--- public/workflow_new.js
+++ public/workflow_new.js
@@ -33,17 +33,13 @@ $(function(){
         <div class="row-fluid">
         <div class="span12">
             <div id="div_c8659880539611e68c9bb888e335e00a" class="control-group">
-                <label class="control-label"><span
-                        class="required">*</span>信托登记要素</label>
+                <label class="control-label"><span class="required">*</span>信托登记要素</label>
                 <div class="controls" id="divVal_c8659880539611e68c9bb888e335e00a">
                     <button type="button" onclick="chYdj('/node/preProductInfo.html','预登记')">预登记</button> 
-                   <!-- <button type="button" onclick="chYdj('/node/preProductInfoDetail.html','预登记详情')">预登记详情</button> -->
                     <button type="button" onclick="chYdj('/node/initProductInfo.html','初始变更更正登记')">初始变更更正登记</button>
-                   <!-- <button type="button" onclick="chYdj('/node/initProductInfoDetail.html','初始登记详情')">初始登记详情</button> -->
                     <button type="button" onclick="chYdj('/node/finishProductInfo.html','终止登记')">终止登记</button>
-                  <!--  <button type="button" onclick="chYdj('/node/finishProductInfoDetail.html','终止登记详情')">终止登记详情</button> -->
                     <button type="button" onclick="chYdj('/node/reportProductInfo.html','事前报告登记')">事前报告登记</button>
-                  <!--  <button type="button" onclick="chYdj('/node/reportProductInfoDetail.html','事前报告登记详情')">事前报告登记详情</button> -->
+					<span>&nbsp;&nbsp;<font color=red size=3>注意:信托登记四种类型,每个审批流程只能够提交一种登记类型.</font></span>
                     <div id="mdldiv_frame" class="modal container hide fade" tabindex="-1" role="dialog" aria-labelledby="myModal_frameContainer" aria-hidden="true">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
